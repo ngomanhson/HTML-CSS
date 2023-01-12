529df585b3b13e3a0f0b9a73bb81e2b0d406b1c6
NgoManhSon <ngomanhson2004txpt@gmail.com>
2023-01-12 21:31:28 +0700

    Countdown-Animated

---
+++ Countdown-Animated/script.js
@@ -0,0 +1,22 @@
+const nums = document.querySelectorAll('.nums span');
+const counter = document.querySelector('.counter');
+const finalMessage = document.querySelector('.final');
+
+runAnimation();
+
+function runAnimation() {
+	nums.forEach((num, idx) => {
+		const penultimate = nums.length - 1;
+		num.addEventListener('animationend', (e) => {
+			if(e.animationName === 'goIn' && idx !== penultimate){
+				num.classList.remove('in');
+				num.classList.add('out');
+			} else if (e.animationName === 'goOut' && num.nextElementSibling){
+				num.nextElementSibling.classList.add('in');
+			} else {
+				counter.classList.add('hide');
+				finalMessage.classList.add('show');
+			}
+		});
+	});
+}
