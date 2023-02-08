a2e4866003c0fbbdb641aa632f3cd347d7f8a443
NgoManhSon <ngomanhson2004txpt@gmail.com>
2023-02-08 11:28:12 +0700

    Verify OTP Updates

--- OTP/script.js
+++ OTP/script.js
@@ -1,45 +1,39 @@
-const inputs = document.querySelectorAll(".otp-field input");
-inputs.forEach((input, index) => {
-    input.dataset.index = index;
-    input.addEventListener("paste", handleOnPasteOtp);
-    input.addEventListener("keyup", handleOtp);
+const inputs = document.querySelectorAll("input"),
+button = document.querySelector("button");
+
+inputs.forEach((input, index1) => {
+input.addEventListener("keyup", (e) => {
+  
+  const currentInput = input,
+    nextInput = input.nextElementSibling,
+    prevInput = input.previousElementSibling;
+
+  if (currentInput.value.length > 1) {
+    currentInput.value = "";
+    return;
+  }
+  
+  if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
+    nextInput.removeAttribute("disabled");
+    nextInput.focus();
+  }
+
+  if (e.key === "Backspace") {
+    inputs.forEach((input, index2) => {
+      if (index1 <= index2 && prevInput) {
+        input.setAttribute("disabled", true);
+        input.value = "";
+        prevInput.focus();
+      }
+    });
+  }
+
+  if (!inputs[3].disabled && inputs[3].value !== "") {
+    button.classList.add("active");
+    return;
+  }
+  button.classList.remove("active");
+});
 });
 
-function handleOnPasteOtp(e) {
-    const data = e.clipboardData.getData("text");
-    const value = data.split("");
-    if (value.length == inputs.length) {
-        inputs.forEach((input, index) => (input.value = value[index]));
-        submit();
-    }
-}
-
-function handleOtp(e) {
-    const input = e.target;
-    let value = input.value;
-    input.value = "";
-    input.value = value ? value[0] : "";
-
-    let fieldIndex = input.dataset.index;
-    if (value.length > 0 && fieldIndex < inputs.length - 1) {
-        input.nextElementSibling.focus();
-    }
-
-    if (e.key == "Backspace" && fieldIndex > 0) {
-        input.previousElementSibling.focus();
-    }
-
-    if (fieldIndex == inputs.length - 1) {
-        submit();
-    }
-}
-
-function submit() {
-    let otp = "";
-    inputs.forEach((input) => {
-        otp += input.value;
-        input.disabled = true;
-        input.classList.add("disabled");
-    });
-    console.log(otp);
-}
+window.addEventListener("load", () => inputs[0].focus());
